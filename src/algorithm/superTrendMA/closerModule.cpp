73d0b3f9ff0adf77f3dc08e93b0458213d2f7706
Maksim Isakov <maksim.isakov@mytona.com>
2022-05-26 00:35:19 +0300

    finish closer logic

--- src/algorithm/superTrendMA/closerModule.cpp
+++ src/algorithm/superTrendMA/closerModule.cpp
@@ -7,16 +7,53 @@ using namespace algorithm;
 closerModule::closerModule(stMAlgorithm& aAlgorithm)
 	:algorithm(aAlgorithm) {}
 
+
 bool closerModule::check() {
 	if (algorithm.getState() == getIntState(eBaseState::LONG)) {
 		if (utils::isLessOrEqual(algorithm.getCandle().low, algorithm.getOrder().getStopLoss())) {
-			algorithm.closeOrder();
+			algorithm.closeOrder(); // TO DO ADD CLOSE PRICE
+			return true;
+		}
+
+		if (checkStates(true)) {
+			algorithm.closeOrder(); // TO DO ADD CLOSE PRICE
+			return true;
 		}
 	}
 	else if (algorithm.getState() == getIntState(eBaseState::SHORT)) {
 		if (utils::isGreaterOrEqual(algorithm.getCandle().high, algorithm.getOrder().getStopLoss())) {
-			algorithm.closeOrder();
+			algorithm.closeOrder(); // TO DO ADD CLOSE PRICE
+			return true;
+		}
+
+		if (checkStates(false)) {
+			algorithm.closeOrder(); // TO DO ADD CLOSE PRICE
+			return true;
 		}
 	}
 	return false;
+}
+
+void closerModule::updateState(bool& aState, bool aAdd) const {
+	if (algorithm.getData().getCloserConjuctionCheck()) {
+		aState &= aAdd;
+	}
+	else {
+		aState |= aAdd;
+	}
+}
+
+bool closerModule::checkStates(bool aLong) const {
+	bool checkState = algorithm.getData().getCloserConjuctionCheck();
+	if (algorithm.getData().getCloserTrendChangeCheck()) {
+		auto trendState = algorithm.getIndicators().isSuperTrendUp();
+		trendState = (aLong) ? !trendState : trendState;
+		updateState(checkState, trendState);
+	}
+	if (algorithm.getData().getCloserMainMACheck()) {
+		auto mainMAState = algorithm.getMAModule().isFirstUp();
+		mainMAState = (aLong) ? !mainMAState : mainMAState;
+		updateState(checkState, mainMAState);
+	}
+	return checkState;
 }
--- src/algorithm/superTrendMA/closerModule.h
+++ src/algorithm/superTrendMA/closerModule.h
@@ -7,7 +7,11 @@ namespace algorithm {
 	public:
 		closerModule(stMAlgorithm& aAlgorithm);
 		bool check();
+
 	private:
+		void updateState(bool& aState, bool aAdd) const;
+		bool checkStates(bool aLong) const;
+
 		stMAlgorithm& algorithm;
 	};
 }
